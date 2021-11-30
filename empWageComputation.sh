423087209c12afa3e7d16b5307c419e296a0d81b
unknown <>
2021-11-30 13:23:14 +0530

    [udit] modifiying the empwage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,5 +1,6 @@
 #! /bin/bash/ -x
-echo "welcome"
+
+echo "Welcome"
 
 isPresent=$((RANDOM%3))
 valueForPresent=1
@@ -12,29 +13,31 @@ empWorkedHour=0
 totalSalary=0
 empWage=0
 
-if [ $isPresent -eq $valueForPresent ]
-then
-        echo "Emp is present."
-        empWorkedHour=$FullDayWorkingHour
-        empWage=$wagePerHour
-else
-        if [ $isPresent -eq $valueForPartTime ]
-        then
-                echo "Emp is  doing part time"
-                empWorkedHour=$FullDayPartTimeWorkingHour
-                empWage=$wagePerForPatTime
-        else
-                echo "Emp is absent."
-        fi
-fi
+case $isPresent in 
+	$valueForPresent)
+		echo "Emp is present."
+		empWorkedHour=$FullDayWorkingHour
+		empWage=$wagePerHour
+	;;
+	$valueForPartTime)
+		echo "Emp is doing part time"
+		empWorkedHour=$FullDayPartTimeWorkingHour
+		empWage=$wagePerForPatTime
+	;;
+	*)
+		echo "Emp is absent."
+	;;
+esac
+
 function CalculatEmpWage(){
-        empWorked=$1
-        wagePerHour=$2
-        echo $(($empWorked*$wagePerHour))
+	empWorked=$1
+	wagePerHour=$2
+	echo $(($empWorked*$wagePerHour))
 }
+
 if [ $isPresent -eq $valueForPresent ] || [ $isPresent -eq $valueForPartTime ]
 then
-        totalSalary=`CalculatEmpWage $empWorkedHour $empWage`
+	totalSalary=`CalculatEmpWage $empWorkedHour $empWage`	
 fi
 
 echo "Total Salary for one day "$totalSalary
